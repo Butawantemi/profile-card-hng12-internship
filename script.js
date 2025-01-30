000eb3564eb263a6f23a787a6848b422d2305e1c
Butawantemi <japhetbuta@gmail.com>
2025-01-30 16:04:14 +0300

    profile-card

--- script.js
+++ script.js
@@ -1,7 +1,8 @@
+/* This script will update the time on the page every second. */
 
 const updateTime =() => {
     const utcTimeElement = document.getElementById("utc-time");
     const now = new Date();
-    utcTimeElement.textContent = `Current UTC Time: ${now.toUTCString()}`;
+    utcTimeElement.textContent = `Current UTC Time: ${now}`;
 }
 updateTime();
